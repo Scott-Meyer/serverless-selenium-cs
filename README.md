4d14f88ec5080efd4c269d45809954ab981d1229
Scott Meyer <Scott.R.Meyer@gmail.com>
2018-08-23 14:22:55 -0600

    final.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # serverless-selenium-cs
-attampt to make a docker which does selenium stuff in c# (eventually lambda?)
+
+This project allows you to test and create selenium c# AWSLambda projects.
+
+
+# howto
+Requires: linux (windows linux subsystem) with dotnet and maketools. Docker.
+
+Clone repository
+browse to repository
+```bash
+make fetch-dependencies
+make dotnet
+make docker-build
+make docker-run
+make build-lambda-package
+```
+Note: if you know how to make it not re-download the bin/ packages if they are already there... please help.
+
+note2: If you are on windows, just exit the bash for the docker parts, and manually paste the lines into cmd.
+
+# uploading
+If you have not already, run `make build-lambda-package` for a build.zip
+
+Create new .netcore 2.1 lambda function and upload zip file.
+
+# running
+I ran this using the AWSSDK.net and passing a json payload with the 3 required fields which are 'url','height','width'.
--- src/Function.cs
+++ src/Function.cs
@@ -4,11 +4,13 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -39,28 +41,79 @@ namespace serverless_selenium_cs
     public class Othercode
     {
         private flambda input;
-        public Othercode(flambda input)
+        public Othercode(flambda inputs)
         {
-
+            input = inputs;
         }
         public string run()
         {
-            IWebDriver driver = get_driver();
-            Console.WriteLine("Got Driver");
+            // Orientation matters mostly for proper chart sizing. We open up 2 chrome processes, one for landscape and one for portrait.
+            string currentOrientation = int.Parse(input.width) >= 1000 ? ".Landscape" : "Portrait";
+            string strWidth;
+            string strHeight;
+            DetermineSizeFromOrientation(currentOrientation, out strWidth, out strHeight);
 
-            driver.Url = "https://www.google.com/";
-            IWebElement element = driver.FindElement(By.Name("q"));
-            element.SendKeys("21 buttons");
-            element.Submit();
+            IWebDriver driver = null;
+            try
+            {
+                string host = new Uri(input.url).Host;
 
-            //driver.FindElement(By.Name("btnK")).Click();
-            System.Threading.Thread.Sleep(5000);
+                // Start up the web driver.
+                driver = get_driver(input.width, input.height);
+                driver.Navigate().GoToUrl(input.url);
+                // Allow the dashboards to load, to allow the web report sections done increment or decrement the counter.
+                Thread.Sleep(3000);
+            //    cancellationToken.ThrowIfCancellationRequested();
+                // Make the chrome driver wait a little bit longer than the request cancellation time out period,
+                // thus when the chrome driver times out, the request must also be timed out too.
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(32));
+                // This loop ensures that the report is *really* ready.
+                bool isReady = false;
+                while (!isReady)
+                {
+                    try
+                    {
+                        wait.Until(d => (bool)((d as IJavaScriptExecutor).ExecuteScript("return window.webReportSectionsDone") ?? false) == true); // Wait for our page to tell us we are done loading.
+                    }
+                    catch (WebDriverTimeoutException)
+                    {
+                        // Catch timeout exceptions here as this means that at least one of the sections times out while loading.
+                        // If one section times out in a report, the entire report should not be generated.
+            //            cancellationToken.ThrowIfCancellationRequested();
+                    }
+                    catch (InvalidOperationException) { } // Catch invalid operation exception if the page is not loaded yet but code need to access 'webReportSectionsDone'
 
-            string first_google_result_title = driver.FindElement(By.XPath("(//div[@class=\"rc\"]//a)[1]")).GetAttribute("innerHTML");
+                    Thread.Sleep(500); // Wait half a second to ensure we're done processing
+                    // Check if still ready. If still ready, then exit loop.
+                    IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                    isReady = (bool)js.ExecuteScript("return window.webReportSectionsDone");
+            //        cancellationToken.ThrowIfCancellationRequested();
+                }
 
-            return first_google_result_title;
+                //Get source
+                string outputContents = driver.PageSource;
+
+                driver.Navigate().GoToUrl(new Uri(input.url).GetLeftPart(UriPartial.Authority) + "/#/logout"); // Initiate a logout request so the next time we use this instance, we don't reuse the session.
+
+                return outputContents;
+            }
+            finally
+            {
+                // Clean up driver resources
+                if (driver != null)
+                {
+                    driver.Quit();
+                }
+            }
         }
-        public IWebDriver get_driver()
+
+        /// <summary>
+        /// A function to get a IWebDriver while in AWS land.
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public IWebDriver get_driver(string width, string height)
         {
             ChromeOptions chrome_options = new ChromeOptions();
 
@@ -89,7 +142,7 @@ namespace serverless_selenium_cs
             chrome_options.AddArgument("--headless");
             chrome_options.AddArgument("--no-sandbox");
             chrome_options.AddArgument("--disable-gpu");
-            chrome_options.AddArgument("--window-size=1280x1696");
+            chrome_options.AddArgument(string.Format("--window-size={0}x{1}", width, height));
             chrome_options.AddArgument(string.Format("--user-data-dir={0}", _tmp_folder + "/user-data"));
             chrome_options.AddArgument("--hide-scrollbars");
             chrome_options.AddArgument("--enable-logging");
@@ -108,6 +161,27 @@ namespace serverless_selenium_cs
 
             //Return the driver.
             return driver;
+        }
+
+        /// <summary>
+        /// Determine the width and height for the report from report orientation.
+        /// </summary>
+        /// <param name="orientation">Report orientation</param>
+        /// <param name="strWidth"></param>
+        /// <param name="strHeight"></param>
+        public static void DetermineSizeFromOrientation(string orientation,
+            out string strWidth, out string strHeight)
+        {
+            // These numbers were based off the sizes set in the css theme files.
+            // Some may be slightly tweaked for the web.
+            // The first set is landscape orientation, second is portrait.
+            // For the portrait theme labels to display correctly in PDF report,
+            // and in order to render the chart in a proper size for portrait theme,
+            // the minimum width should be 1428 for both one-column and two-column report layout.
+            // CHANING THIS WILL AFFECT THE WEB PDF REPORT CHART SIZE GREATLY.
+            // SO PLEASE DO NOT CHANGE THIS UNLESS YOU HAVE A GOOD REASON.
+            strWidth = orientation == "Landscape" ? "1932" : "1428";
+            strHeight = orientation == "Landscape" ? "1428" : "1932";
         }
     }
 }
